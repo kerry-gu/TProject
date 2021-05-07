3fc8f1b496583934d659654d361f8837c5ed24ad
kerry-gu <549520210@qq.com>
2021-05-07 09:58:23 +0800

    updata

--- ftp/tests/test_1.py
+++ ftp/tests/test_1.py
@@ -2,16 +2,21 @@ import json
 import pytest
 import yaml
 import requests as re
+import logging
 from util.commonlib import get_test_data
 cases, list_params = get_test_data("./data/api.yaml")
 class TestInPost():
     @pytest.mark.parametrize('case,http',list(list_params), ids=cases)
     def test_1(self,case,http,env):
+        log = logging.getLogger('test1')
         with re.session() as s:
             _json = s.post(
                 url = env["host"]["host"]+http['path'],
                 headers = http['headers'],
                 json = http['body']
                 )
-        return _json
-        assert _json.status_code == 200
+            print(_json)
+            log.info(_json)
+            log.debug(_json)
+            # return _json 
+            assert _json.status_code == 200
